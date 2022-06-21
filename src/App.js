0cf292f6f7a5fe7546e1ac6f889a4db3e7d4bda1
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2022-06-21 20:22:41 +0530

    theme reducer added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ChatPage, DefaultLayout, LoginPage, PostsPage } from "./pages";
+import { useDispatch, useSelector } from "react-redux";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./themes";
 function App() {
+  const { currentTheme } = useSelector((store) => store.theme);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to={"/chats"} />} />
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/" element={<DefaultLayout />}>
-          <Route path="/chats" element={<ChatPage />}></Route>
-          <Route path="/posts" element={<PostsPage />}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={currentTheme === "light" ? lightTheme : darkTheme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to={"/chats"} />} />
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/" element={<DefaultLayout />}>
+            <Route path="/chats" element={<ChatPage />}></Route>
+            <Route path="/posts" element={<PostsPage />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/app/configureStore.js
+++ src/app/configureStore.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
+import themeReducer from "../features/themeSlice";
 export default configureStore({
   reducer: {
     auth: authReducer,
+    theme: themeReducer,
   },
 });
