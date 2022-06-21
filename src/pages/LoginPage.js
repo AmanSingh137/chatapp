05cff7240c31653fc45444939e33303f42e19ddf
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2022-06-21 20:50:50 +0530

    loginpage initalized

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [username, setUsername] = useState("");
+  const login = () => {};
+  return (
+    <div>
+      <form action="" onSubmit={login}>
+        <h2>Username</h2>
+        <input type="text" onChange={(e) => setUsername(e.target.value)} />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
