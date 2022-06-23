8d0b82653a6081f05d06cb79a0c6c8677ee3a735
AmanSingh137 <65694984+AmanSingh137@users.noreply.github.com>
2022-06-23 17:23:34 +0530

    Initialised posts bar

--- src/pages/PostsPage.js
+++ src/pages/PostsPage.js
@@ -1,7 +1,45 @@
 import React from "react";
+import styled from 'styled-components';
+import AddIcon from '@mui/icons-material/Add';
+import { Button } from "@mui/material";
 
 const PostsPage = () => {
-  return <div>PostsPage</div>;
+  return <>
+    <PostsBar>
+      <p>Post Here</p>
+      <PostOptions>
+        <Button>
+          Post
+        </Button>
+        <AddIcon />
+      </PostOptions>
+    </PostsBar>
+  </>;
 };
 
 export default PostsPage;
+
+const PostsBar = styled.div`
+  background-color: grey;
+  height: 9.375rem;
+  width: 75rem;
+  margin-left: 9.375rem;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  > p {
+    padding-left: 20px;
+    padding-top: 10px;
+  }
+  
+`;
+
+const PostOptions = styled.div`
+  margin-top: 50px;
+  background-color: red;
+  
+  > button {
+    background-color: green;
+  }
+`;
