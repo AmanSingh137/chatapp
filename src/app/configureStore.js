ea12a2be5a931abc4924d4089271f88d17db6451
AmanSingh137 <65694984+AmanSingh137@users.noreply.github.com>
2022-06-23 23:37:41 +0530

    Posts slice created

--- src/app/configureStore.js
+++ src/app/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import themeReducer from "../features/themeSlice";
+import postsReducer from "../features/postsSlice";
 export default configureStore({
   reducer: {
     auth: authReducer,
     theme: themeReducer,
+    posts: postsReducer
   },
 });
--- src/pages/PostsPage.js
+++ src/pages/PostsPage.js
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from 'styled-components';
 import AddIcon from '@mui/icons-material/Add';
 import { Button } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux/es/exports";
+import { useNavigate } from "react-router-dom";
+import { setPosts } from "../features/postsSlice";
 
 const PostsPage = () => {
+  const [postText, setPostsText] = useState("");
+  const [images, setImages] = useState(null);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { currentPosts } = useSelector(
+    (store) => store.posts
+  );
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    dispatch(setPosts(
+      [ ...currentPosts]
+    ))
+  }
+
+  const handleClickAdd = (e) => {
+    e.preventDefault();
+
+  }
+
   return <>
     <PostsBar>
       <p>Post Here</p>
+      <input type="text" onChange={ (e) => {
+        setPostsText(e.target.value);
+      } } />
       <PostOptions>
-        <Button>
+        <Button onClick={handleClick}>
           Post
         </Button>
-        <AddIcon />
+        <AddIcon onClick={handleClickAdd} />
       </PostOptions>
     </PostsBar>
   </>;
@@ -32,12 +58,16 @@ const PostsBar = styled.div`
     padding-left: 20px;
     padding-top: 10px;
   }
-  
+  > input {
+    width: 50rem;
+    height: 3.125rem;
+    margin-top: 20px;
+  }
 `;
 
 const PostOptions = styled.div`
   margin-top: 50px;
-  background-color: red;
+  //background-color: red;
   
   > button {
     background-color: green;
