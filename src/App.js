01cb2fcd185c8f77c835f14a987b433265fc5ca9
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2022-06-30 21:28:21 +0530

    fixed untracked files

--- src/App.js
+++ src/App.js
@@ -1,37 +1,48 @@
 import "./App.css";
+import React, { Suspense } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { ChatPage, DefaultLayout, LoginPage, PostsPage } from "./pages";
 import { useDispatch, useSelector } from "react-redux";
 import { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme } from "./themes";
 import PrivateRoute from "./Routes/privateRoute";
+
+//containers
+const DefaultLayout = React.lazy(() => import("./pages/DefaultLayout"));
+
+//pages
+const ChatPage = React.lazy(() => import("./pages/ChatPage"));
+const LoginPage = React.lazy(() => import("./pages/LoginPage"));
+const PostsPage = React.lazy(() => import("./pages/PostsPage"));
+
 function App() {
   const { currentTheme } = useSelector((store) => store.theme);
   const { user } = useSelector((store) => store.auth);
   return (
-    <ThemeProvider theme={currentTheme === "light" ? lightTheme : darkTheme}>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<PrivateRoute />}>
-            {user !== null && (
-              <Route path="/" element={<Navigate to={"/chats"} />} />
-            )}
+    <Suspense>
+      <ThemeProvider theme={currentTheme === "light" ? lightTheme : darkTheme}>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<PrivateRoute />}>
+              {user !== null && (
+                <Route path="/" element={<Navigate to={"/chats"} />} />
+              )}
 
-            {/* <Route path="/login" element={<LoginPage />}></Route> */}
-            <Route path="/" element={<DefaultLayout />}>
-              <Route path="/chats" element={<ChatPage />}></Route>
-              <Route path="/posts" element={<PostsPage />}></Route>
+              {/* <Route path="/login" element={<LoginPage />}></Route> */}
+              <Route path="/" element={<DefaultLayout />}>
+                <Route path="/chats" element={<ChatPage />}></Route>
+                <Route path="/posts" element={<PostsPage />}></Route>
+              </Route>
             </Route>
-          </Route>
 
-          <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="*"
-            element={<Navigate to={user === null ? "/login" : `/chats`} />}
-          />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+            <Route path="/login" element={<LoginPage />} />
+            <Route
+              path="*"
+              element={<Navigate to={user === null ? "/login" : `/chats`} />}
+            />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </Suspense>
   );
 }
 
